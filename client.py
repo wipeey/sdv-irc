0d403e3ad8290458df7bf0b2c30b2e7fe188e3a7
wipeey <>
2024-10-14 23:16:55 +0200

    Implement multiple clients connections; message broadcasting; fixing minor connectivity problems; better error handling

--- client.py
+++ client.py
@@ -2,6 +2,7 @@
 import socket
 import threading
 
+# Print the received messages from the server
 def receive_messages(s):
     while True:
         data = s.recv(1024)
@@ -12,18 +13,28 @@ def receive_messages(s):
             print(data.decode())
 
 
+# Send a message to the server
 def send_messages(s):
     while True:
-        to_send = input("Prompt: ")
+        to_send = input()
         s.send(to_send.encode())
 
 
+# Connect to the server and start listening for messages
 def main():
     host = 'localhost'
     port = 1234
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
+    
+    try:
+        s.connect((host, port))
+    except OSError:
+        print(f'Unable to connect to {host}:{port}. Exiting...')
+        exit(1)
+    except ConnectionRefusedError:
+        print(f'Connection to {host}:{port} refused. Exiting...')
+        exit(1)
 
     send_msg = threading.Thread(target=send_messages, args=(s,))
     recv_msg = threading.Thread(target=receive_messages, args=(s,))
--- server.py
+++ server.py
@@ -7,32 +7,69 @@ from time import sleep
 host = 'localhost'
 port = 1234
 
+connected_clients = []
 
 # Executes whenever a client connects to the server
 def handle_client(conn, addr):
+    global connected_clients
+
     client_ip = addr[0]
     client_port = addr[1]
+
     with conn:
-        print(f'{client_ip}:{client_port} connected to the server')
+        broadcast_message(f'{client_ip}:{client_port} connected to the server'.encode(), conn)
+
         while True:
             data = conn.recv(1024)
             if not data:
-                print(f'{client_ip}:{client_port} disconnected')
-                break
+                broadcast_message(f'{client_ip}:{client_port} disconnected'.encode())
+                try:
+                    connected_clients.remove(conn) # Remove the client from the list
+                    break
+                except Exception:
+                    pass
             else:
-                print(data.decode())
+                broadcast_message(data, conn)
 
 
-# Setting up listener
-# Starting core threads
+# Send a message to all connected clients except the sender
+def broadcast_message(message, sender=None):
+    global connected_clients
+
+    print(message.decode()) # Log the received message to the console
+
+    for client in connected_clients:
+        try:
+            if client != sender:  # Don't send the message to the sender
+                client.send(message)
+                # print(f'Sent message to client {client[1]}')
+        except:
+            connected_clients.remove(client)
+
+
+# Return the current number of connected clients to the server
+def get_connected_amount():
+    global connected_clients
+    return len(connected_clients)
+
+ 
+ # Server loop that listens for new connections and handles them
 def main():
+    global connected_clients
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # SO_REUSEADDR allows the port to be reused after the server is stopped and restarted
     s.bind((host, port))
+
+    print(f'Server started on {host}:{port}')
 
     while True:
         s.listen()
 
         conn, addr = s.accept()
+
+        connected_clients.append(conn)
+        print(get_connected_amount(), 'clients connected') # DEBUG
 
         connection_thread = threading.Thread(target=handle_client, args=(conn, addr))
         connection_thread.start()
