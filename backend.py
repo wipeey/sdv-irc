918810bae6a6757904eb1793ab0812188144242a
Fraawdrinn <chatoui.wael40@gmail.com>
2024-10-16 21:05:56 +0200

    Added working client and started connexion with server

--- backend.py
+++ backend.py
@@ -2,46 +2,43 @@
 import socket
 import threading
 
-# Print the received messages from the server
-def receive_messages(s):
+# Function to receive messages from the server
+def receive_messages(s, update_chat_callback):
     while True:
-        data = s.recv(1024)
-        if not data:
-            print("Connection lost!")
+        try:
+            data = s.recv(1024)
+            if not data:
+                update_chat_callback("Connection lost!")
+                break
+            else:
+                update_chat_callback(data.decode())
+        except ConnectionResetError:
+            update_chat_callback("Server disconnected!")
             break
-        else:
-            print(data.decode())
 
+# Function to send messages to the server
+def send_messages(s, message):
+    try:
+        s.send(message.encode())
+    except Exception as e:
+        print(f"Error sending message: {e}")
 
-# Send a message to the server
-def send_messages(s):
-    while True:
-        to_send = input()
-        s.send(to_send.encode())
-
-
-# Connect to the server and start listening for messages
-def main():
-    host = 'localhost'
-    port = 1234
-
+# Function to connect to the server
+def connect_to_server(host='localhost', port=1234, update_chat_callback=None):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+
     try:
         s.connect((host, port))
-    except OSError:
-        print(f'Unable to connect to {host}:{port}. Exiting...')
-        exit(1)
-    except ConnectionRefusedError:
-        print(f'Connection to {host}:{port} refused. Exiting...')
-        exit(1)
+    except (OSError, ConnectionRefusedError):
+        if update_chat_callback:
+            update_chat_callback(f"Unable to connect to {host}:{port}")
+        return None
 
-    send_msg = threading.Thread(target=send_messages, args=(s,))
-    recv_msg = threading.Thread(target=receive_messages, args=(s,))
+    if update_chat_callback:
+        update_chat_callback(f"Connected to {host}:{port}")
 
-    send_msg.start()
-    recv_msg.start()
+    # Start a thread to listen for incoming messages
+    recv_thread = threading.Thread(target=receive_messages, args=(s, update_chat_callback))
+    recv_thread.start()
 
-
-if __name__ == '__main__':
-    main()
+    return s  # Return the socket to be used in `client.py` for sending messages
